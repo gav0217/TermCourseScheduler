c2f55c96710e92a9e8bbadd7e221abdd608203b0
Gavril <gav217@yahoo.com>
2023-01-16 19:59:36 -0800

    A

--- app/src/main/java/com/gfofiu/c196project/userinterface/TermDetails.java
+++ app/src/main/java/com/gfofiu/c196project/userinterface/TermDetails.java
@@ -9,17 +9,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.gfofiu.c196project.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class TermScheduler extends AppCompatActivity {
+public class TermDetails extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_term_scheduler);
-        FloatingActionButton fab = findViewById(R.id.floatingActionButton);
+        setContentView(R.layout.activity_term_details);
+        FloatingActionButton fab = findViewById(R.id.floatingActionButton2);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(TermScheduler.this, TermDetails.class);
+                Intent intent = new Intent(TermDetails.this, CourseDetails.class);
                 startActivity(intent);
             }
         });
